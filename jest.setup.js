9a8a2390ad22e902d70fe939cce21220f6a0b27d
SophiaDWokoma <148252375+SophiaDWokoma@users.noreply.github.com>
2024-05-09 16:43:10 +0100

    add config file for jest and add setup for jest-axe config

---
+++ jest.setup.js
@@ -0,0 +1,14 @@
+const { configureAxe } = require("jest-axe");
+
+// Configure Axe for accessibility testing
+const axe = configureAxe({
+  rules: {
+    "color-contrast": { enabled: true },
+    "skip-link": { enabled: true },
+    "html-has-lang": { enabled: true },
+    "link-name": { enabled: true }
+  }
+});
+
+// Make Axe globally available for all tests
+global.axe = axe;
