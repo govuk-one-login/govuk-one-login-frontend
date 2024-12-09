237219584a6c757a7ea69348eea34eead9c1c3fc
SophiaDWokoma <148252375+SophiaDWokoma@users.noreply.github.com>
2024-12-09 16:56:36 +0000

    fix(DFC-581): add error handling for adding/setting lang param in the URL

--- packages/frontend-language-toggle/src/language-param-setter.ts
+++ packages/frontend-language-toggle/src/language-param-setter.ts
@@ -1,4 +1,11 @@
-export default function addLanguageParam(language: string, url: URL) {
+export default function addLanguageParam(language: string, url?: URL) {
+  if (!url) {
+    console.warn(
+      "URL is undefined. The parameter cannot be added, and the toggle will not work.",
+    );
+    return "#invalid-url-lang-toggle";
+  }
+
   url.searchParams.set("lng", language);
   return url.pathname + url.search;
 }
