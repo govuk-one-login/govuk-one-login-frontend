70e8217d4d2b1f7ebe5cbdd5b3239c2f6eca7df5
Joanneyoung01 <jo.young@digital.cabinet-office.gov.uk>
2025-01-09 12:00:57 +0000

    test(716): add set logger test

--- packages/frontend-passthrough-headers/src/__test__/specs/logger.spec.ts
+++ packages/frontend-passthrough-headers/src/__test__/specs/logger.spec.ts
@@ -1,26 +1,52 @@
 import { getLogger, setLogger, CustomLogger } from "../../utils/logger";
 
-describe("getLogger", () => {
+jest.mock("pino");
+
+describe("getLogger funcioality", () => {
   let consoleWarnSpy: jest.SpyInstance;
+
   const customLogger: CustomLogger = {
     trace: jest.fn(),
     warn: jest.fn(),
   };
+
   beforeEach(() => {
     consoleWarnSpy = jest.spyOn(console, "warn").mockImplementation(() => {});
   });
+
   afterEach(() => {
     consoleWarnSpy.mockRestore();
     jest.resetModules();
   });
+
   it("should return console when no logger is set", () => {
     const logger = getLogger();
     expect(consoleWarnSpy).toHaveBeenCalledWith("Warning: Logger is undefined");
     expect(logger).toBe(console);
   });
+
   it("should return the custom logger when set", () => {
     setLogger(customLogger);
     const logger = getLogger();
     expect(logger).toBe(customLogger);
   });
+});
+
+describe("setLogger", () => {
+  it("sets the logger as custom logger if provided", () => {
+    const customLogger = {
+      trace: jest.fn(),
+      warn: jest.fn()
+    };
+  
+    setLogger(customLogger);
+    const logger = getLogger();
+
+    logger.trace("Custom trace message");
+    expect(customLogger.trace).toHaveBeenCalledWith("Custom trace message");
+  
+    logger.warn("Custom warn message");
+    expect(customLogger.warn).toHaveBeenCalledWith("Custom warn message");
+  });
+  
 });
