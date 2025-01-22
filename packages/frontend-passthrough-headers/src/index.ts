1ade36254d5b6cf0c0d9d61692c787ed65e9a444
Joanneyoung01 <jo.young@digital.cabinet-office.gov.uk>
2025-01-22 13:50:34 +0000

    feat(716): add custom logger functionality to frontend-passthrough-headers

--- packages/frontend-passthrough-headers/src/index.ts
+++ packages/frontend-passthrough-headers/src/index.ts
@@ -1,8 +1,9 @@
 import { type Request } from "express";
-import { logger, CustomLogger } from "./utils/logger";
+import { getLogger } from "./utils/logger";
 import { processUserIP } from "./utils/userIP";
 import { APIGatewayProxyEvent } from "aws-lambda";
 import { getHeader } from "./utils/getHeader";
+export { setCustomLogger } from "./utils/logger";
 
 const HEADERS = {
   HEADER_TXMA: "txma-audit-encoded",
@@ -21,34 +22,42 @@ export interface PersonalDataHeaders {
  *
  * @param {string} url - The downstream url this request is being sent on to.
  * @param {object} req - A node HTTP/Express type request.
- * @param {object} customLogger - A custom logger.
  * @returns {PersonalDataHeaders}
  */
 export function createPersonalDataHeaders(
   url: string,
   req: Request | APIGatewayProxyEvent,
-  customLogger?: CustomLogger
 ): PersonalDataHeaders {
   const domain = new URL(url).hostname;
   const personalDataHeaders: PersonalDataHeaders = {};
 
   const txmaAuditEncodedHeader = getHeader(req, HEADERS.HEADER_TXMA) as string;
-  const loggerToUse = customLogger || logger;
+  const logger = getLogger();
 
   if (txmaAuditEncodedHeader) {
     personalDataHeaders[HEADERS.HEADER_TXMA] = txmaAuditEncodedHeader;
-    loggerToUse.trace(
+    logger.trace(
       `Personal Data header "${HEADERS.HEADER_TXMA}" is being forwarded to domain "${domain}"`,
     );
   }
 
-  const userIP = processUserIP(req,loggerToUse);
+  const userIP = processUserIP(req);
   if (userIP) {
     personalDataHeaders[HEADERS.OUTBOUND_FORWARDED_HEADER] = userIP;
-    loggerToUse.trace(
+    logger.trace(
       `Personal Data header "${HEADERS.OUTBOUND_FORWARDED_HEADER}" is being forwarded to domain "${domain}"`,
     );
   }
 
   return personalDataHeaders;
 }
+
+/**
+ * This function extracts request headers that should be passed through
+ * to INTERNAL backends. These headers contain Personal Data. They should NEVER
+ * be forwarded to external services.
+ *
+ * @param {string} url - The downstream url this request is being sent on to.
+ * @param {object} req - A node HTTP/Express type request.
+ * @returns {PersonalDataHeaders}
+ */
