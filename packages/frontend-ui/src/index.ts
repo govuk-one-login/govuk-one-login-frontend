c3fa6ac7927c7d95e09e71468a643cc20b1d3d72
Joanneyoung01 <jo.young@digital.cabinet-office.gov.uk>
2025-02-12 16:17:26 +0000

    feat(734): insert loadtraslations function

--- packages/frontend-ui/src/index.ts
+++ packages/frontend-ui/src/index.ts
@@ -1,1 +1,35 @@
-export * as lib from "./lib";
+/* eslint-disable @typescript-eslint/no-require-imports */
+
+import i18next from "i18next";
+import path from 'path';
+
+export const loadFrontendUiTranslations = () => {
+  const nodeModules = (modulePath: string) =>
+    `${path.resolve(__dirname, "../../../node_modules/", modulePath)}`;
+  
+  const nodeModuleLocalesPath = path.join(
+    nodeModules("@govuk-one-login/frontend-ui/components/cookie-banner/locales"),
+  );
+
+  console.log(__dirname);
+  
+  i18next.addResourceBundle(
+    "en",
+    "translation",
+    require(
+      path.join(path.join(nodeModuleLocalesPath, "en", `translation.json`)),
+    ),
+    true,
+    false,
+  );
+  
+  i18next.addResourceBundle(
+    "cy",
+    "translation",
+    require(
+      path.join(path.join(nodeModuleLocalesPath, "cy", `translation.json`)),
+    ),
+    true,
+    false,
+  );
+};
