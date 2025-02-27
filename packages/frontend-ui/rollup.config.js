5af8afffd278e5eadd42bc5a4ecafd0b9bcdd504
DavidMcMichaelGov <david.mcmichael@digital.cabinet-office.gov.uk>
2025-02-27 16:27:22 +0000

    feat(746): updated css imports

--- packages/frontend-ui/rollup.config.js
+++ packages/frontend-ui/rollup.config.js
@@ -1,8 +1,9 @@
 import typescript from "rollup-plugin-typescript2";
 import copy from "rollup-plugin-copy";
 import json from '@rollup/plugin-json';
+import scss from 'rollup-plugin-scss';
 
-export default {
+export default [{
   external: ["pino"],
   input: "src/index.ts",
   output: [
@@ -25,9 +26,22 @@ export default {
           src: "./components/",
           dest: "./build/",
         },
+
+        
       ],
       hook: "closeBundle",
     }),
     json()
   ],
-};
+},
+{
+  input: "./src/all.scss",
+  output:
+    {
+      file: "build/all.css",
+    },
+  plugins: [
+    scss({ fileName: 'all.css'}),
+  ]
+}
+];
