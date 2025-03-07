fd39cfcc741c4134f7d73bcfa9bfb74cea18bb34
nickhealGDS <nick.heal@digital.cabinet-office.gov.uk>
2025-03-07 14:08:24 +0000

    Revert "feat(748): migrating language toggle to frontend-ui"
    This reverts commit 9b3cb3a832ae763937475c2840cea2b95aa0998a.

--- packages/frontend-ui/src/index.ts
+++ packages/frontend-ui/src/index.ts
@@ -3,8 +3,6 @@ import translationCookieBannerCy from "../components/cookie-banner/locales/cy/tr
 import translationCookieBannerEn from "../components/cookie-banner/locales/en/translation.json";
 import translationHeaderCy from "../components/header/locales/cy/translation.json";
 import translationHeaderEn from "../components/header/locales/en/translation.json";
-import welshLanguageToggleCy from "../components/welsh-language-toggle/locales/cy/translation.json";
-import welshLanguageToggleEn from "../components/welsh-language-toggle/locales/en/translation.json";
 
 export const setFrontendUiTranslations = (instanceI18n: typeof i18next) => {
   instanceI18n.addResourceBundle(
@@ -35,20 +33,6 @@ export const setFrontendUiTranslations = (instanceI18n: typeof i18next) => {
     true,
     false,
   );
-  instanceI18n.addResourceBundle(
-    "en",
-    "translation",
-    welshLanguageToggleEn,
-    true,
-    false,
-  );
-  instanceI18n.addResourceBundle(
-    "cy",
-    "translation",
-    welshLanguageToggleCy,
-    true,
-    false,
-  );
 };
 
 export const frontendUiMiddleware = (
@@ -61,15 +45,3 @@ export const frontendUiMiddleware = (
   res.locals.translations = req.i18n.store.data[req.i18n.language];
   next();
 };
-
-export default function addLanguageParam(language: string, url?: URL) {
-  if (!url) {
-    console.warn(
-      "URL is undefined. The parameter cannot be added, and the toggle will not work.",
-    );
-    return "#invalid-url-lang-toggle";
-  }
-
-  url.searchParams.set("lng", language);
-  return url.pathname + url.search;
-}
