7e943bb372e07e0472f92057aed19c6be2cfc726
arwatGDS <arwa.tawawala@digital.cabinet-office.gov.uk>
2025-03-12 12:56:36 +0000

    feat(DFC-810): Integrating ThumbmarkJS into the one login frontend alpha app

--- packages/alpha-app/src/app.js
+++ packages/alpha-app/src/app.js
@@ -90,6 +90,11 @@ app.use(
   "/ga4-assets",
   express.static(nodeModules("@govuk-one-login/frontend-analytics/lib")),
 );
+/** ThumbmarkJS fingerprint **/
+app.use(
+  "/fingerprint",
+  express.static(nodeModules("@govuk-one-login/frontend-device-intelligence/build/esm")),
+);
 app.use(
   session({
     secret: sessionId, // Should I make this more secure?
