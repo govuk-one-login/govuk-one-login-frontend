b8f293bb9cd49a9536c454250d4afaa9d143496d
arwatGDS <arwa.tawawala@digital.cabinet-office.gov.uk>
2025-04-03 16:07:20 +0100

    feat(DFC-824): Corrected tests - WIP

--- packages/alpha-app/test/browser/features/v1/ fingerprint/thumbmark.feature
+++ packages/alpha-app/test/browser/features/v1/ fingerprint/thumbmark.feature
@@ -1,13 +1,15 @@
 @deviceintelligence
 Feature: ThumbmarkJS E2E testing in Alpha App
 
-Scenario: Check that the fingerprint and cookie are set
-    Given I am an FEC dev
-    When I run an E2E test in the alpha app
-    Then I accept analytics cookies from the banner
-    And the cookie has been set
+    Scenario: User does not have the di-device-intelligence cookie
+        Given I visit the welcome page
+        When I reject analytics cookies
+        Then the di-device-intelligence cookie has not been set
 
-Scenario: Check cookie persistence after page refreshes
-    Given I am an FEC dev
-    When I run an E2E refresh test in the alpha app
-    Then the cookie should be reset in the web page
+    Scenario: User has the di-device-intelligence cookie
+        Given I visit the welcome page
+        When I accept analytics cookies
+        Then the di-device-intelligence cookie has been set
+
+Scenario: User has identical cookie value after page refresh
+
--- packages/alpha-app/test/browser/step_definitions/thumbmark.steps.js
+++ packages/alpha-app/test/browser/step_definitions/thumbmark.steps.js
@@ -1,34 +1,16 @@
 const { Given, When, Then } = require("@cucumber/cucumber");
 const { expect } = require("@playwright/test");
 
-/* requisites for scenarios */
-Given("I am an FEC dev", async function() {
-    console.log("Running as FEC dev");
-});
-
 /* Scenario 1 */
-When("I run an E2E test in the alpha app", async function() {
-    await this.page.goto("http://localhost:3000/welcome");
-});
-
-Then("I accept analytics cookies from the banner", async function () {
-    const acceptButton = await this.page.locator("[name=cookiesAccept]");
-    await acceptButton.click();
-});
-
-Then("the cookie has been set", async function() {
+Then("the {word} cookie has been set", async function(cookieName) {
     const cookies = await this.page.context().cookies();
-    const fingerprintCookie = cookies.find(cookie => cookie.name === "di-device-intelligence");
-    expect(fingerprintCookie).toBeDefined();
+    const expectedCookie = cookies.find(cookie => cookie.name === cookieName);
+    expect(expectedCookie).toBeDefined();
 });
 
 /* Scenario 2 */
-When("I run an E2E refresh test in the alpha app", async function() {
-    await this.page.reload();
-});
-
-Then("the cookie should be reset in the web page", async function() {
+Then("the {word} cookie has not been set", async function(cookieName) {
    const cookies = await this.page.context().cookies();
-   const fingerprintCookie = cookies.find(cookie => cookie.name === "di-device-intelligence");
-   expect(fingerprintCookie).toBeUndefined(); 
+   const expectedCookie = cookies.find(cookie => cookie.name === cookieName);
+   expect(expectedCookie).toBeUndefined(); 
 });
