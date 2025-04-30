675d3c6896dee8894b7838b1a13691d1cd6085f6
DavidMcMichael-GDS <david.mcmichael@digital.cabinet-office.gov.uk>
2025-04-30 11:43:57 +0100

    feat(838): updated env to read as boolean value

--- packages/alpha-app/src/config/nunjucks.js
+++ packages/alpha-app/src/config/nunjucks.js
@@ -3,16 +3,18 @@ const nunjucks = require("nunjucks");
 const i18next = require("i18next");
 const frontendUi = require("@govuk-one-login/frontend-ui");
 
+
 module.exports = {
   configureNunjucks: (app, viewsPath) => {
     const nunjucksEnv = nunjucks.configure(viewsPath, {
       autoescape: true,
       express: app,
       noCache: true,
-    }).addGlobal("May_2025_Rebrand", process.env.May_2025_Rebrand);
+    });
 
     nunjucksEnv.addGlobal("addLanguageParam", frontendUi.addLanguageParam);
     nunjucksEnv.addGlobal("contactUsUrl", frontendUi.contactUsUrl);
+    nunjucksEnv.addGlobal("May_2025_Rebrand", process.env.May_2025_Rebrand == 'true');
 
     nunjucksEnv.addFilter("translate", function (key, options) {
       const translate = i18next.getFixedT(this.ctx.language);
