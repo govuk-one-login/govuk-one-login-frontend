ad36abc89a3b7f2fbba73b63e742e5aa14f12965
arwatGDS <arwa.tawawala@digital.cabinet-office.gov.uk>
2025-06-10 16:04:38 +0100

    feat(DFC-863): WIP on tests

--- packages/frontend-ui/test/jestHelper.ts
+++ packages/frontend-ui/test/jestHelper.ts
@@ -2,10 +2,14 @@ import nunjucks from "nunjucks";
 import { JSDOM } from "jsdom";
 import path from "path";
 
-const nunjucksEnv = nunjucks.configure(path.resolve(__dirname, "../../packages/frontend-ui/components"), {
+const componentsPath = path.resolve(__dirname, "../../frontend-ui/components");
+console.log(componentsPath);
+const nunjucksEnv = nunjucks.configure(componentsPath, {
   autoescape: true,
   noCache: true,
 });
+
+nunjucksEnv.addGlobal('componentsPath', componentsPath);
 
 export function render(macroName: string, params = {}) {
   if (Object.keys(params).length === 0 && params.constructor === Object) {
@@ -15,16 +19,18 @@ export function render(macroName: string, params = {}) {
   }
 
   const macroParams = JSON.stringify(params, null, 2);
-  const macroPath = `./${path.relative(process.cwd(), path.resolve(__dirname, "./../components"))}`;
-  let macroString = `{% from "${macroPath}/${macroName}/macro.njk" import ${macroName} %}`;
-  macroString += `{{ ${macroName}(${macroParams}) }}`;
-  
-  console.log("PATH", macroPath);
-  console.log("NAME:", macroName);
 
+  const macroPath = `./${macroName}`;
+  let macroString = `{% from "${macroName}/macro.njk" import ${macroName} %}`;
+  macroString += `{{ ${macroName}(${macroParams}) }}`;
+
+  console.log("COMPONENTS PATH", componentsPath); 
+  console.log("MACRO PATH", macroPath);
+  console.log("NAME:", macroName);
   console.log("STRING:", macroString);
 
-  const output = nunjucks.renderString(macroString, {});
-  const dom = new JSDOM(output);
-  return dom.window.document;
+
+const output = nunjucksEnv.renderString(macroString, params);
+const dom = new JSDOM(output);
+return dom.window.document;
 }
