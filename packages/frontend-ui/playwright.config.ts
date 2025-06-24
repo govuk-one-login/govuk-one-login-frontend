305a8ebff09dfa1329e3b14d664d83b6b08fec9c
arwatGDS <arwa.tawawala@digital.cabinet-office.gov.uk>
2025-06-24 14:06:08 +0100

    feat(DFC-775): npm command fixed. New snapshots generated

---
+++ packages/frontend-ui/playwright.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "@playwright/test";
+
+export default defineConfig({
+    testDir: "browser-tests/functional-tests",
+    use: {
+        baseURL: "http://localhost:3000",
+        screenshot: "only-on-failure",
+        trace: "on-first-retry",
+    },
+    projects: [
+        {
+            name: 'chromium',
+            use: { browserName: "chromium" }
+        }
+    ],
+});
