ab48a396fe22b2b7a74c15b00f81f2ac4c17b4ee
arwatGDS <arwa.tawawala@digital.cabinet-office.gov.uk>
2025-06-27 11:39:49 +0100

    feat(DFC-912): WIP regression test step

--- packages/frontend-ui/playwright.config.ts
+++ packages/frontend-ui/playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
         trace: "on-first-retry",
         viewport: { width: 1280, height: 720 },
         deviceScaleFactor: 1,
-        headless: true
+        headless: false
     },
     projects: [
         {
