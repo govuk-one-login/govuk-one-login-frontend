336caf326af265ee7b94089f7eb9edc57afa8c33
arwatGDS <arwa.tawawala@digital.cabinet-office.gov.uk>
2025-06-27 12:02:51 +0100

    feat(DFC-912): WIP regression testing step

--- packages/frontend-ui/playwright.config.ts
+++ packages/frontend-ui/playwright.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
     testDir: "browser-tests/functional-tests",
+    retries: process.env.CI ? 2 : 0,
     use: {
         baseURL: "http://localhost:3000",
         screenshot: "only-on-failure",
         trace: "on-first-retry",
         viewport: { width: 1280, height: 720 },
         deviceScaleFactor: 1,
-        headless: false
+        headless: true,
     },
     projects: [
         {
