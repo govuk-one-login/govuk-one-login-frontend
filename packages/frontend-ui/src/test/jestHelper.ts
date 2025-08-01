ae1336dabed220de0afa7e1274d8f2ce22ad9d81
nickhealGDS <nick.heal@digital.cabinet-office.gov.uk>
2025-08-01 12:03:07 +0100

    test: add mobile base template banner tests

--- packages/frontend-ui/src/test/jestHelper.ts
+++ packages/frontend-ui/src/test/jestHelper.ts
@@ -3,13 +3,15 @@ import { JSDOM } from "jsdom";
 import path from "path";
 import { addLanguageParam, contactUsUrl } from "../index";
 
-const frontendUIComponentsPath = path.resolve(__dirname, "../../components");
 const govukComponentsPath = path.resolve(
   __dirname,
   "../../../../node_modules/govuk-frontend",
 );
+const frontendUIComponentsPath = path.resolve(__dirname, "../../components");
+const frontendUIBuildPath = path.resolve(__dirname, "../../../");
+const stubs = path.resolve(__dirname, "./stubs");
 const nunjucksEnv = nunjucks.configure(
-  [frontendUIComponentsPath, govukComponentsPath],
+  [frontendUIComponentsPath, govukComponentsPath, frontendUIBuildPath, stubs],
   {
     autoescape: true,
     noCache: true,
@@ -17,13 +19,24 @@ const nunjucksEnv = nunjucks.configure(
 );
 nunjucksEnv.addGlobal("addLanguageParam", addLanguageParam);
 nunjucksEnv.addGlobal("contactUsUrl", contactUsUrl);
-nunjucksEnv.addGlobal("May_2025_Rebrand", false);
+nunjucksEnv.addGlobal("MAY_2025_REBRAND_ENABLED", true);
+nunjucksEnv.addFilter("translate", function () {
+  return "";
+});
 
 export function render(macroFolder: string, macroName: string, params = {}) {
   const macroParams = JSON.stringify(params, null, 2);
 
   let macroString = `{% from "${macroFolder}/macro.njk" import ${macroName} %}`;
   macroString += `{{ ${macroName}(${macroParams}) }}`;
+
+  const output = nunjucksEnv.renderString(macroString, params);
+  const dom = new JSDOM(output);
+  return dom.window.document;
+}
+
+export function renderTemplate(macroPath: string, params = {}) {
+  const macroString = `{% extends "bases/${macroPath}" %}`;
 
   const output = nunjucksEnv.renderString(macroString, params);
   const dom = new JSDOM(output);
