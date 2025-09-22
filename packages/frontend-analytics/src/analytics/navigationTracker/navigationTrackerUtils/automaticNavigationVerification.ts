0218f2c95a50c269fb285ed45b32714dd9a1b590
ug-gds <umma.gohil@digital.cabinet-office.gov.uk>
2025-09-22 15:28:32 +0100

    feat(dfc-949): data attributes for button script starter

---
+++ packages/frontend-analytics/src/analytics/navigationTracker/navigationTrackerUtils/automaticNavigationVerification.ts
@@ -0,0 +1,28 @@
+/**
+ * Determine whether the button can be located 
+ * @param element - The HTML element to check
+ * @returns {boolean} - Returns true if the element is a button, false otherwise.
+ */
+
+export const isButton = (element: HTMLElement): boolean => {
+  const button = document.getElementsByTagName("button")[0];
+  return button?.contains(element);
+}
+
+/**
+ * Determine if data attribute data-nav is present on the element
+ * @param element - The HTML element to check
+ * @returns {boolean} - Returns true if the data attribute is present, false otherwise.
+ */
+export const hasDataNavAttribute = (element: HTMLElement): boolean => {
+  return element.hasAttribute('data-nav');
+}
+
+/**
+ * Determine if data attribute data-link is present on the element
+ * @param element - The HTML element to check
+ * @returns {boolean} - Returns true if the data attribute is present, false otherwise.
+ */
+export const hasDataLinkAttribute = (element: HTMLElement): boolean => {
+  return element.hasAttribute('data-link');
+}
