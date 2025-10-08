7c96ade02428432c1d10b369d3a5c5bdd0e147bf
ug-gds <umma.gohil@digital.cabinet-office.gov.uk>
2025-10-08 13:19:40 +0100

    feat(dfc-973): dummy basic components

--- packages/alpha-app/app/page.tsx
+++ packages/alpha-app/app/page.tsx
@@ -4,8 +4,58 @@ export default function Page() {
   return (
     <>
       {/* skip link */}
-
+      <p className="govuk-body">
+        To view the skip link component tab to this example, or click inside
+        this example and press tab.
+      </p>
+      <a href="#" className="govuk-skip-link" data-module="govuk-skip-link">
+        Skip to main content
+      </a>
       {/* cookie banner */}
+      <div
+        className="govuk-cookie-banner"
+        data-nosnippet
+        role="region"
+        aria-label="Cookies on [name of service]"
+      >
+        <div className="govuk-cookie-banner__message govuk-width-container">
+          <div className="govuk-grid-row">
+            <div className="govuk-grid-column-two-thirds">
+              <h2 className="govuk-cookie-banner__heading govuk-heading-m">
+                Cookies on [name of service]
+              </h2>
+              <div className="govuk-cookie-banner__content">
+                <p className="govuk-body">
+                  We use some essential cookies to make this service work.
+                </p>
+                <p className="govuk-body">
+                  We’d also like to use analytics cookies so we can understand
+                  how you use the service and make improvements.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="govuk-button-group">
+            <button
+              type="button"
+              className="govuk-button"
+              data-module="govuk-button"
+            >
+              Accept analytics cookies
+            </button>
+            <button
+              type="button"
+              className="govuk-button"
+              data-module="govuk-button"
+            >
+              Reject analytics cookies
+            </button>
+            <a className="govuk-link" href="#">
+              View cookies
+            </a>
+          </div>
+        </div>
+      </div>
 
       {/* header  */}
       <header className="govuk-header" data-module="govuk-header">
