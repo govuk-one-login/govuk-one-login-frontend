bbd688fc6907ef9b094b3b91ea4077501508d712
ug-gds <umma.gohil@digital.cabinet-office.gov.uk>
2025-11-17 14:29:02 +0000

    fix(dfc-1039): issues with es modules and server

--- packages/alpha-app/src/server.js
+++ packages/alpha-app/src/server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import { Liquid } from "liquidjs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
