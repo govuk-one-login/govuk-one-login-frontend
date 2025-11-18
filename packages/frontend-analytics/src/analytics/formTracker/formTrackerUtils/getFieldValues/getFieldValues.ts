f840a89aaa0fd4a8b46e83b3e42a7e783d64dd51
nickhealGDS <nick.heal@digital.cabinet-office.gov.uk>
2025-11-18 15:37:29 +0000

    refactor: simplify pii removal logic

--- packages/frontend-analytics/src/analytics/formTracker/formTrackerUtils/getFieldValues/getFieldValues.ts
+++ packages/frontend-analytics/src/analytics/formTracker/formTrackerUtils/getFieldValues/getFieldValues.ts
@@ -1,5 +1,3 @@
-import { FormField } from "../../formTracker.interface";
-
 export const getElementValue = (element: HTMLInputElement): string => {
   const label = document.querySelector(`label[for="${element.id}"]`);
   return label?.textContent?.trim() || "undefined";
@@ -12,27 +10,6 @@ export const getElementValue = (element: HTMLInputElement): string => {
  */
 export const getFormElement = (): HTMLFormElement | null => {
   return document.querySelector("#main-content form");
-};
-
-/**
- * Retrieves the field value from an array of form fields.
- *
- * @param {FormField[]} elements - The array of form fields.
- * @return {string} The concatenated field values separated by a comma (if there is more than one field).
- */
-export const getFieldValue = (elements: FormField[]): string => {
-  let value = "";
-  const separator = elements.length > 1 ? ", " : "";
-  elements.forEach((element) => {
-    if (
-      element.type === "checkbox" ||
-      element.type === "radio" ||
-      element.type === "select-one"
-    ) {
-      value += element.value + separator;
-    }
-  });
-  return value;
 };
 
 /**
