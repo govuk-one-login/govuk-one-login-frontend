f55b266ebbad87ebb058175e870799b87a3d8853
DavidMcMichael-GDS <david.mcmichael1@digital.cabinet-office.gov.uk>
2026-01-13 16:30:40 +0000

    feat(1066): initial setup of event-catalogue-utils

---
+++ packages/event-catalogue-utils/rollup.config.js
@@ -0,0 +1,19 @@
+import typescript from "rollup-plugin-typescript2";
+
+export default {
+    external: ["pino"],
+    input: "src/index.ts",
+    output: [
+      {
+        file: "build/cjs/index.cjs",
+        format: "cjs",
+      },
+      {
+        file: "build/esm/index.js",
+        format: "es",
+      },
+    ],
+    plugins: [
+      typescript(),
+    ]
+};
