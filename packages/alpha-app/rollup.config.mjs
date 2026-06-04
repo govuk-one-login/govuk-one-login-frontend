9888a2c38772cc03c9104b4c732c1039da54bdae
ug-gds <umma.gohil@digital.cabinet-office.gov.uk>
2026-06-04 10:38:47 +0100

    fix(dfc-1223): alpha app build failing

--- packages/alpha-app/rollup.config.mjs
+++ packages/alpha-app/rollup.config.mjs
@@ -1,30 +1,10 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
 
-export default [
-  {
-    input: "src/frontend-assets/javascript/index.js",
-    plugins: [nodeResolve()],
-    output: [
-      {
-        file: "public/javascript/index.js",
-        format: "iife",
-      },
-    ],
+export default {
+  input: "src/frontend-assets/javascript/index.js",
+  plugins: [nodeResolve()],
+  output: {
+    file: "public/javascript/index.js",
+    format: "iife",
   },
-  {
-    input: "testValidateEvent.js",
-    plugins: [
-      nodeResolve({
-        preferBuiltins: false,
-        browser: true,
-      }),
-      commonjs(),
-    ],
-    output: {
-      file: "dist/k6-event-validator.bundle.js",
-      format: "es",
-    },
-    external: ["k6", "k6/http"],
-  },
-];
+};
