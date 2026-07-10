1d63b8cf4f3f7a4242c5035ce18b5168d78f0bb7
nickhealGDS <nick.heal@digital.cabinet-office.gov.uk>
2026-07-10 11:35:15 +0100

    fix(DFC-1312): handle tracking relative urls in form submissions

--- packages/frontend-analytics/src/utils/dataScrapersUtils/dataScrapers.test.ts
+++ packages/frontend-analytics/src/utils/dataScrapersUtils/dataScrapers.test.ts
@@ -1,36 +1,71 @@
 import { isChangeLink, getDomain, getDomainPath } from "./dataScrapers";
 
-test("Get domain from element url", () => {
-  const location = getDomain(
-    "https://signin.account.gov.uk/enter-email-create",
-  );
-  expect(location).toEqual("https://signin.account.gov.uk");
+describe("getDomain", () => {
+  test("Get domain from absolute url", () => {
+    const location = getDomain(
+      "https://signin.account.gov.uk/enter-email-create",
+    );
+    expect(location).toEqual("https://signin.account.gov.uk");
+  });
+
+  test("Get domain from relative url", () => {
+    const location = getDomain("/enter-email-create");
+    expect(location).toEqual("http://localhost:3000");
+  });
+
+  test("Getdomain needs to return undefined if url = undefined", () => {
+    const location = getDomain("undefined");
+    expect(location).toEqual("undefined");
+  });
 });
 
-test("Getdomain needs to return undefined if url = undefined", () => {
-  const location = getDomain("undefined");
-  expect(location).toEqual("undefined");
-});
+describe("getDomainPath", () => {
+  test("Get url path from 0 to 500 max from element url", () => {
+    const location = getDomainPath(
+      "https://signin.account.gov.uk/enter-email-create",
+      0,
+    );
+    expect(location).toEqual("/enter-email-create");
+  });
 
-test("Get url path from 0 to 500 max from element url", () => {
-  const location = getDomainPath(
-    "https://signin.account.gov.uk/enter-email-create",
-    0,
-  );
-  expect(location).toEqual("/enter-email-create");
-});
+  test("Get url path longer than 500 characters", () => {
+    // 499 characters because a '/' is appended to the start when we fetch the path
+    const longUrlFirst499Characters =
+      "DPtnYonMCTeWdtVDhroFHMqqIGEoICSMcIuoJvPDnXWqwefwNejqzEZoxxiXdgfpQdWVWsGtEHOGYtwBVlFxFEqGxIhvkQMqMDdGgTWrWkHeCWTzhmKsfWZttoEoRiBfbCYIxDHQubdknmLdzexXCnearPrqIfjCBXgDsoQMjonbPtjYUIgthMdrLjIWcDnmXyveOFREJAIaXfPGqcRIhfkxpRVNiFevDBjqIGjHAZXysfgyxYahBjOzfHJPYPidKbQDEJxmWDkfUGKGKTUKpBJixXxBdtpHeLJhYvVzynFPkLFSEHpsToygMESyGjSKEsxbnqDDnvrcaTHGbpDWPHpWlVyAHpQqGTcBbCLPrdOZLxUGWrDMTEhrbfMTnObSaOdHQQEwEaavHuETnqCVHZnSqrXuaSJgCPrJPylXVnyQhLjxQTlsbXfsJtRhjdrdKikvWoiiixETbGqvVofTGOkwwlBTsHgylNpdpkspyIgshZzJgLz";
+    const longUrlLast4Characters = "test";
 
-test("Get domain path needs to return undefined if url = undefined", () => {
-  const location = getDomainPath("undefined", 0);
-  expect(location).toEqual("undefined");
-});
+    const location1 = getDomainPath(
+      longUrlFirst499Characters + longUrlLast4Characters,
+      0,
+    );
+    const location2 = getDomainPath(
+      longUrlFirst499Characters + longUrlLast4Characters,
+      1,
+    );
 
-test("Get undefined if url path part is not found from element url", () => {
-  const location = getDomainPath(
-    "https://signin.account.gov.uk/enter-email-create",
-    1,
-  );
-  expect(location).toEqual("undefined");
+    expect(location1).toEqual(
+      "/DPtnYonMCTeWdtVDhroFHMqqIGEoICSMcIuoJvPDnXWqwefwNejqzEZoxxiXdgfpQdWVWsGtEHOGYtwBVlFxFEqGxIhvkQMqMDdGgTWrWkHeCWTzhmKsfWZttoEoRiBfbCYIxDHQubdknmLdzexXCnearPrqIfjCBXgDsoQMjonbPtjYUIgthMdrLjIWcDnmXyveOFREJAIaXfPGqcRIhfkxpRVNiFevDBjqIGjHAZXysfgyxYahBjOzfHJPYPidKbQDEJxmWDkfUGKGKTUKpBJixXxBdtpHeLJhYvVzynFPkLFSEHpsToygMESyGjSKEsxbnqDDnvrcaTHGbpDWPHpWlVyAHpQqGTcBbCLPrdOZLxUGWrDMTEhrbfMTnObSaOdHQQEwEaavHuETnqCVHZnSqrXuaSJgCPrJPylXVnyQhLjxQTlsbXfsJtRhjdrdKikvWoiiixETbGqvVofTGOkwwlBTsHgylNpdpkspyIgshZzJgLz",
+    );
+    expect(location2).toEqual("test");
+  });
+
+  test("Get domain path needs to return undefined if url = undefined", () => {
+    const location = getDomainPath("undefined", 0);
+    expect(location).toEqual("undefined");
+  });
+
+  test("Get undefined if url path part is not found from element url", () => {
+    const location = getDomainPath(
+      "https://signin.account.gov.uk/enter-email-create",
+      1,
+    );
+    expect(location).toEqual("undefined");
+  });
+
+  test("Get domain from relative url", () => {
+    const location = getDomainPath("/enter-email-create", 0);
+    expect(location).toEqual("/enter-email-create");
+  });
 });
 
 describe("should check for changeLink", () => {
